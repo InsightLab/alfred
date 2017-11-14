00b7bfffc4d91e156f34301f7c0ba396c0263478
Lucas Peres <lucaspg96@live.com>
2017-11-14 09:47:26 -0300

    [#1] Correcting tests

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,47 +1,72 @@
 from os import sys,environ
-
 sys.path.append("..")
+
+import unittest
+from telegram.utils.request import Request
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+
+from ptbtest import ChatGenerator, MessageGenerator, Mockbot, UserGenerator
 
 from alfredbot.BotConstructor import BotConstructor
 from alfredbot.Blueprint import Blueprint
 
-#first, we must define the token of the bot
-token = environ.get("BOT_ALFRED_TOKEN")
+class SimpleTest(unittest.TestCase):
+	def setUp(self):
+		# For use within the tests we nee some stuff. Starting with a Mockbot
+		self.bot = Mockbot()
+		self.constructor = BotConstructor(bot=self.bot)
+		# Some generators for users and chats
+		self.ug = UserGenerator()
+		self.cg = ChatGenerator()
+		# And a Messagegenerator and updater (for use with the bot.)
+		self.mg = MessageGenerator()
+		# self.updater = Updater(bot=self.bot)
 
-#then, we can create the BotConstructor object
-bot = BotConstructor(token)
+		def start(bot,update):
+			self.start_value = True
 
-#we can define a function to handle updates on the bot
-def start(bot,update):
-	#the bot paramether represents an object from python-telegram-bot Bot class
-	#update paramether represents an object from python-telegram-bot Update class
-	update.message.reply_text("Bot started")
+		self.constructor.add_command_handler("start",start)
 
-#then, we can add this function as a handler to the command "start"
-bot.add_command_handler("start",start)
+		blueprint = Blueprint()
 
-#we can also create a blueprint object, that will contains the organization of the handlers
-blueprint = Blueprint()
+		def ping(bot,update):
+			self.ping_value = True
 
-#now we can define another function to handler a command
-def ping(bot,update):
-	update.message.reply_text("Pong!")
+		def msg(bot,update):
+			self.message_value = update.message.text
 
-#here we define a command that will handler filters (the default is text, but can't be changed yet)
-def msg(bot,update):
-	update.message.reply_text(update.message.text)
+		blueprint.add_command_handler("ping",ping)
+		blueprint.add_message_handler(msg)
 
-#here we have the error handler, that will catch ONLY ERRORS FROM THE BOT. Python error will not be caught.
-def error(bot,update,error):
-	print(error)
+		self.constructor.add_blueprint(blueprint)
 
-#now, we set this handlers on the blueprint
-blueprint.add_command_handler("ping",ping)
-blueprint.add_message_handler(msg)
-blueprint.set_error_handler(error)
+	def test_start(self):
+		self.constructor.start()
+		update = self.mg.get_message(text="/start")
+		self.bot.insertUpdate(update)
 
-#and add the blueprint to the BotConstructor
-bot.add_blueprint(blueprint)
+		self.assertEqual(self.start_value, True)
 
-#now, we just need to start the pool
-bot.start()
+		self.constructor.stop()
+
+	def test_ping(self):
+		self.constructor.start()
+		update = self.mg.get_message(text="/ping")
+		self.bot.insertUpdate(update)
+
+		self.assertEqual(self.ping_value, True)
+
+		self.constructor.stop()
+
+	def test_message(self):
+		self.constructor.start()
+		update = self.mg.get_message(text="alfred is a cool name!")
+		self.bot.insertUpdate(update)
+
+		self.assertEqual(self.message_value, "alfred is a cool name!")
+
+		self.constructor.stop()
+
+
+if __name__ == '__main__':
+    unittest.main()
