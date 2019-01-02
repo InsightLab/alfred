a8af3b7e13989bbb34902c0e9feb4d7e3c278093
Lucas Peres <lucaspg96@live.com>
2019-01-02 09:41:54 -0300

    Replacing clean blueprint by callback query

--- alfred.py
+++ alfred.py
@@ -5,9 +5,11 @@ from alfredbot.exceptions.UserNotFoundException import UserNotFoundException
 
 from alfredbot.blueprints.users_blueprint import users_blueprint
 from alfredbot.blueprints.workstations_blueprint import workstations_blueprint
-from alfredbot.blueprints.clean_blueprint import clean_blueprint
+
+from telegram.ext import CallbackQueryHandler
 
 from os import environ
+from datetime import datetime as dt
 
 token = environ.get("BOT_ALFRED_TOKEN")
 
@@ -40,12 +42,32 @@ def start(bot,update):
 	with open("texts/start_text.txt") as f:
 			update.message.reply_text(f.read())
 
+def clean_button(bot,update):
+	query = update.callback_query
+
+	obj = query.data
+
+	user = query.from_user
+
+	text = "Obrigado {}.\nManutenção registrada!\n".format(user.username, obj)
+
+	bot.edit_message_text(text=query.message.text+"\nUPDATE: limpo por "+user.username,
+						  chat_id=query.message.chat_id,
+						  message_id=query.message.message_id
+						  )
+	
+	with open("{}_cleaning_history.txt".format(obj), 'a') as f:
+		f.write("{},{}\n".format(dt.now(),user.username))
+
+	bot.sendMessage(chat_id=query.message.chat_id, text=text)
+
 bot.add_command_handler("start",start)
 bot.add_command_handler("help",help)
 
+bot.dp.add_handler(CallbackQueryHandler(clean_button))
+
 bot.add_blueprint(users_blueprint)
 bot.add_blueprint(workstations_blueprint)
-bot.add_blueprint(clean_blueprint)
 
 bot.start()
 
